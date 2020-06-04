072e9b562d090ec9ac69d99f480fc35d2fc47247
Kundan Patel <kundanpatel@Kundans-MBP.fios-router.home>
2020-06-03 21:00:36 -0400

    Javascript

---
+++ Introduction/Projects/DogWeather.js
@@ -0,0 +1,20 @@
+var myAge = 23;// age mine
+
+var earlyYears = 2;// early
+
+earlyYears *= 10.5;
+
+var laterYears = myAge - 2;// later
+
+laterYears *= 4;
+
+console.log(earlyYears, laterYears);
+
+var myAgeInDogYears = earlyYears + laterYears;// dog age
+
+var myName = 'Kundan'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
---
+++ Introduction/Projects/Kelvin.js
@@ -0,0 +1,18 @@
+var kelvin = 0;
+// setting value of kelvin
+
+var celsius = kelvin - 273;// celsius
+
+var fahrenheit = celsius * (9/5) + 32;// fahrenheit
+
+fahrenheit = Math.floor(fahrenheit)// flooring
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+var newton = Math.floor(celsius * (33/100));
+
+console.log(newton);
+
+
+
+
